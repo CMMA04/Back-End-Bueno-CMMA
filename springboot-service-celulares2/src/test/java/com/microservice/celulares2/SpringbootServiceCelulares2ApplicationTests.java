4f5e3c71fb1b1e2b1dd422a9018384632f39e37a
CMMA04 <carlosmn265@gmail.com>
2024-04-16 02:50:43 -0600

    Commit Netflix Eureka Client
    Commit del segundo video de eureka

---
+++ springboot-service-celulares2/src/test/java/com/microservice/celulares2/SpringbootServiceCelulares2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.celulares2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCelulares2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
