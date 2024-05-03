c530b109ec87626bc39107b851be4708ab141935
CMMA04 <carlosmn265@gmail.com>
2024-05-03 01:54:34 -0600

    Commit Exponiendo ID´s
    Commit donde exponemos los ids de los usuarios

---
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package com.microservice.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.microservice.usuarios.entity.Role;
+import com.microservice.usuarios.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter{
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+}
