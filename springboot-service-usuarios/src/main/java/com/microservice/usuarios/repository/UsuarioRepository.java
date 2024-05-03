c42132a16a6900c504fb3024a97d6205183182bc
CMMA04 <carlosmn265@gmail.com>
2024-05-03 17:59:07 -0600

    Commit antes del JAR

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -1,12 +1,13 @@
 package com.microservice.usuarios.repository;
 
 import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import com.microservice.usuarios.entity.Usuario;
+import com.microservice.commons.users.entity.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario,Long>{
